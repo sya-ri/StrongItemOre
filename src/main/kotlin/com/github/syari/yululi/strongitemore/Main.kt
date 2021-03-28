fa030d060b85d0c9e20d700e61875d8a676fc700
sya-ri <34268371+sya-ri@users.noreply.github.com>
2021-03-28 17:03:38 +0900

    Initial commit

---
+++ src/main/kotlin/com/github/syari/yululi/strongitemore/Main.kt
@@ -0,0 +1,14 @@
+package com.github.syari.yululi.strongitemore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class Main : JavaPlugin() {
+    companion object {
+        internal lateinit var plugin: JavaPlugin
+    }
+
+    init {
+        plugin = this
+    }
+}
